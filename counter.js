143865ac1ca5bd2aa6c66cd339656b43978dd48e
Jules Moreno <julesmoreno66@gmail.com>
2022-08-22 14:56:34 -0700

    final commit

--- counter.js
+++ counter.js
@@ -12,6 +12,7 @@ let main = function() {
     resetBtn.addEventListener("click", event => {
         counter = 0
         count.innerText = counter;
+        count.style.color = "black";
     })
 
     // increment
@@ -21,6 +22,7 @@ let main = function() {
         let elementValue = event.target.value;
         counter = incrementDecrement(counter, elementValue);
         count.innerText = counter;
+        count.style.color = "green";
     })
    
     // decrement
@@ -30,6 +32,7 @@ let main = function() {
         let elementValue = event.target.value;
         counter = incrementDecrement(counter, elementValue);
         count.innerText = counter;
+        count.style.color = "red";
     })
 }()
 
@@ -46,7 +49,55 @@ let body = document.body;
 body.style.height = `${window.innerHeight}px`;
 body.style.display = "flex";
 body.style.justifyContent = "center"
-body.style.alignItems = "center"
+// body.style.alignItems = "center"
+body.style.backgroundColor = "rgb(180, 200, 220)"
 
 let mainCounter = document.querySelector(".main");
+mainCounter.style.marginTop = "13em"
 mainCounter.style.textAlign = "center"
+
+// h2
+let h2 = document.getElementsByTagName("h2");
+let h2Array = Array.from(h2);
+
+for (let element of h2Array) {
+    element.style.fontSize = "2.5em"
+}
+
+let title = h2Array[0];
+title.style.fontSize = "3em";
+title.style.fontFamily = "Arial";
+title.style.color = "rgb(100, 80, 180)";
+title.style.textShadow = "1px 1px lightblue";
+title.style.letterSpacing = "10px"
+
+let count = h2Array[1];
+count.style.color = "rgb(30, 30, 10)";
+count.style.textShadow = "1px 1px lightblue";
+
+// button
+let btn = document.querySelectorAll("button");
+let arrayBtn = Array.from(btn);
+
+for (let btn of arrayBtn) {
+    btn.style.padding = "5px 1em"
+    btn.style.fontSize = "1.6em";
+    btn.style.fontWeight = "bold";
+    btn.style.borderWidth = "4px";
+    btn.style.borderColor = "gray";
+    btn.style.borderRadius = "7px";
+    btn.style.boxShadow = "0px 6px 10px white"
+}
+
+// increment button
+let increment = document.querySelector(".increment");
+increment.style.color = "green";
+
+// decrement button
+let decrement = document.querySelector(".decrement");
+decrement.style.color = "red";
+
+// reset button
+let reset = document.querySelector(".reset");
+reset.style.margin = "0 .7em"
+
